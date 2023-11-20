eb52ca0beb857e1dc3d07f3e7c5a96c73d187f77
Sharmitha Yazhini N M <104150250+SharmithaYazhini@users.noreply.github.com>
2023-11-20 18:58:12 -0500

    Update main.py

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ Original file is located at
     https://colab.research.google.com/drive/1uorCbrpv_HfMIZoQrb8dRI6c7CI48fIM
 """
 
-pip install -r requirements.txt
+python install_dependencies.py
 
 import streamlit as st
 
