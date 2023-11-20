34c25df4238b9a7d1b446fbcbf05194c7f3add46
Sharmitha Yazhini N M <104150250+SharmithaYazhini@users.noreply.github.com>
2023-11-20 18:42:24 -0500

    Update main.py

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ Original file is located at
     https://colab.research.google.com/drive/1uorCbrpv_HfMIZoQrb8dRI6c7CI48fIM
 """
 
-
+pip install matplotlib
 import streamlit as st
 
 import pandas as pd
